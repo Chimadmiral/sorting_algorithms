a10fa4750d7cb79164c50adb11dbc30f9393537b
Admiral <110990779+Chimadmiral@users.noreply.github.com>
2023-01-31 16:21:24 +0200

    100 shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void shell_sort(int *array, size_t size) {
+  int interval, i, j, temp;
+  for (interval = 1; interval <= size / 3; interval = interval * 3 + 1) {
+    // do nothing
+  }
+  for (; interval > 0; interval /= 3) {
+    for (i = interval; i < size; i++) {
+      temp = array[i];
+      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+        array[j] = array[j - interval];
+      }
+      array[j] = temp;
+    }
+    printf("Interval = %d: ", interval);
+    for (i = 0; i < size; i++) {
+      printf("%d ", array[i]);
+    }
+    printf("\n");
+  }
+}
